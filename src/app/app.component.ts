1148a64628ae23d7ddd66284f81ccb1e1ce1a7dd
Benny Feng <bfeng@kth.se>
2017-03-08 20:52:06 +0100

    added dynamic list of values using angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,40 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+    <div name="'Object">
+    <ul>
+        
+        <li *ngFor="let value of values; let i = index">
+        <label>Value:{{i+1}} is {{value}} </label>
+          
+        </li>
+        <form (submit)="addValue(thing.value)">
+          
+            <input type="text" #thing/>
+            
+         </form>   
+    </ul>
+    
+   
+    </div>
+
+
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    name: string;
+    values: string[];
+    otherValues: string;
+
+    constructor(){
+        this.name = 'Grodan boll';
+        this.values = ['foo','goo'];
+        this.otherValues = 'idk'
+
+}
+    addValue(newvalue){
+        this.values.push(newvalue);
+    }
+
+}
